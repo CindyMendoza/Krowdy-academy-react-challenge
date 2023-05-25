3413d853c03d1699e0d1a63da18abed0e9407c94
Cindy Mendoza <mendozaic17@gmail.com>
2023-05-25 01:19:17 -0500

    advance with video library

---
+++ src/components/cardVideo.jsx
@@ -0,0 +1,79 @@
+// import * as React from "react";
+// import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+function CardVideo() {
+  const constraints = (window.constraints = {
+    audio: false,
+    video: true,
+  });
+
+  function handleSuccess(stream) {
+    const video = document.querySelector("video");
+    const videoTracks = stream.getVideoTracks();
+    console.log("Got stream with constraints:", constraints);
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream; // make variable available to browser console
+    video.srcObject = stream;
+  }
+
+  function handleError(error) {
+    if (error.name === "OverconstrainedError") {
+      const v = constraints.video;
+      errorMsg(
+        `The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`
+      );
+    } else if (error.name === "NotAllowedError") {
+      errorMsg(
+        "Permissions have not been granted to use your camera and " +
+          "microphone, you need to allow the page access to your devices in " +
+          "order for the demo to work."
+      );
+    }
+    errorMsg(`getUserMedia error: ${error.name}`, error);
+  }
+
+  function errorMsg(msg, error) {
+    const errorElement = document.querySelector("#errorMsg");
+    errorElement.innerHTML += `<p>${msg}</p>`;
+    if (typeof error !== "undefined") {
+      console.error(error);
+    }
+  }
+
+  async function init(e) {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      handleSuccess(stream);
+      e.target.disabled = true;
+    } catch (e) {
+      handleError(e);
+    }
+  }
+
+  /* document
+     .querySelector("#showVideo")
+     .addEventListener("click", (e) => init(e));
+  */
+  return (
+    <>
+      <Card>
+        <CardContent>
+          <video id="gum-local" autoPlay playsInline></video>
+          <div id="errorMsg"></div>
+        </CardContent>
+        <CardActions>
+          <Button size="small" id="showVideo" onClick={(e) => init(e)}>
+            Show Video
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  );
+}
+
+export default CardVideo;
